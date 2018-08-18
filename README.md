1a3732f5eb90b8b2aa6ec32829b71a11ff5b8f37
aeffiong <aeffiong9@gmail.com>
2018-08-17 21:59:18 -0600

    cleaning up code and updating README

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # unit-4-game: Crystals Collector
 
-Welcome to Crystals Collector! In this game, you will match your score with the random number presented. Click the crystals, until your score
-matches the random number. Each crystal is worth a certain amount of points. You will not know how many points each crystal is until you click
-on it. If you match your score with the random number, you win. And the game resets. Enjoy!
+Welcome to Crystals Collector! In this game, you will match your score with the random number presented. Click the crystals, until your score matches the random number. Each crystal is worth a certain amount of points. You will not know how many points each crystal is worth until you click on it. If you match your score with the random number, you win. And the game resets.
+
+## Getting Started
+
+Start the game [here](https://aeffiong.github.io/unit-4-game/).
+
+The purpse of the game is to match your score to the random score presented and keep track of wins and losses as you play.
+
+## Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* Javascript
+* jQuery
+
+## Built with:
+
+* Visual Studio Code
--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,9 +1,9 @@
 // Define variables
-var numToMatch = Math.floor(Math.random() *120) +19;
-var crystalNumberOne = Math.floor(Math.random() *12) +1;
-var crystalNumberTwo = Math.floor(Math.random() *12) +1;
-var crystalNumberThree = Math.floor(Math.random() *12) +1;
-var crystalNumberFour = Math.floor(Math.random() *12) +1;
+var numToMatch = Math.floor(Math.random() * 120) + 19;
+var crystalNumberOne = Math.floor(Math.random() * 12) + 1;
+var crystalNumberTwo = Math.floor(Math.random() * 12) + 1;
+var crystalNumberThree = Math.floor(Math.random() * 12) + 1;
+var crystalNumberFour = Math.floor(Math.random() * 12) + 1;
 // var crystalsArray = [1, 2, 3, 4];
 var wins = 0;
 var losses = 0;
@@ -11,112 +11,103 @@ var score = 0;
 
 
 // start game function: setting the start screen and adding event listener .ready
-$(document).ready(function() {
+$(document).ready(function () {
     $("#random-number").text(numToMatch);
     score = 0;
     $("#user-score").text(score);
-    
-   
 
-    // for(var i = 0; i < crystalsArray.length; i++) {
-    //     crystalsArray[i] = Math.floor(Math.random() *12) +1;
-    //     console.log(crystalsArray[i]);
-    // }
-
-
-
-//   function to reset the game
-      function reset() {
-        numToMatch = Math.floor(Math.random() *120) +19;
+    //   function to reset the game
+    function reset() {
+        numToMatch = Math.floor(Math.random() * 120) + 19;
         $("#random-number").text(numToMatch);
         score = 0;
         $("#user-score").text(score);
-        crystalNumberOne = Math.floor(Math.random() *12) +1;
-        crystalNumberTwo = Math.floor(Math.random() *12) +1;
-        crystalNumberThree = Math.floor(Math.random() *12) +1;
-        crystalNumberFour = Math.floor(Math.random() *12) +1;
-      }
+        crystalNumberOne = Math.floor(Math.random() * 12) + 1;
+        crystalNumberTwo = Math.floor(Math.random() * 12) + 1;
+        crystalNumberThree = Math.floor(Math.random() * 12) + 1;
+        crystalNumberFour = Math.floor(Math.random() * 12) + 1;
+    }
 
     // on click, add each crystal number to score number, check to see if user score = number to match
     // determine if the user won (guess  = number to match) or lost (guess > number to match)
-    $("#crystal1").on("click", function() {
+    $("#crystal1").on("click", function () {
         score = score + crystalNumberOne;
         $("#user-score").text(score);
 
-        if(score === numToMatch) {
+        if (score === numToMatch) {
             wins++;
             alert("You win!! " + score);
             $("#wins").text("Wins: " + wins);
             reset();
         }
-            
-    
-        else if(score > numToMatch) {
+
+
+        else if (score > numToMatch) {
             losses++;
             alert("You lose!! " + score);
             $("#losses").text("Losses: " + losses);
             reset();
-            
-            
+
+
         }
-    
+
     });
-    $("#crystal2").on("click", function() {
+    $("#crystal2").on("click", function () {
         score = score + crystalNumberTwo;
         $("#user-score").text(score);
 
-        if(score === numToMatch) {
+        if (score === numToMatch) {
             wins++;
             alert("You win!! " + score);
             $("#wins").text("Wins: " + wins);
             reset();
         }
-    
-        else if(score > numToMatch) {
+
+        else if (score > numToMatch) {
             losses++;
             alert("You lose!! " + score);
             $("#losses").text("Losses: " + losses);
             reset();
         }
-       
+
     });
-    $("#crystal3").on("click", function() {
+    $("#crystal3").on("click", function () {
         score = score + crystalNumberThree;
         $("#user-score").text(score);
 
-        if(score === numToMatch) {
+        if (score === numToMatch) {
             wins++;
             alert("You win!! " + score);
             $("#wins").text("Wins: " + wins);
             reset();
         }
-    
-        else if(score > numToMatch) {
+
+        else if (score > numToMatch) {
             losses++;
             alert("You lose!! " + score);
             $("#losses").text("Losses: " + losses);
             reset();
         }
-        
+
     });
-    $("#crystal4").on("click", function() {
+    $("#crystal4").on("click", function () {
         score = score + crystalNumberFour;
         $("#user-score").text(score);
 
-        if(score === numToMatch) {
+        if (score === numToMatch) {
             wins++;
             alert("You win!! " + score);
             $("#wins").text("Wins: " + wins);
             reset();
         }
-    
-        else if(score > numToMatch) {
+
+        else if (score > numToMatch) {
             losses++;
             alert("You lose!! " + score);
             $("#losses").text("Losses: " + losses);
             reset();
         }
-         
+
     });
 
 });
